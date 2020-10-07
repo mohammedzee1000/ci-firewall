e2b8ecf45848bef3d14b60c0586ca4841c3ae648
Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>
2020-10-07 16:05:54 +0530

    Adding workdir as param
    Signed-off-by: Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>

--- pkg/cli/worker/work.go
+++ pkg/cli/worker/work.go
@@ -135,6 +135,7 @@ func NewWorkCmd(name, fullname string) *cobra.Command {
 	cmd.Flags().StringVar(&o.runScript, "run", os.Getenv(messages.RequestParameterRunScript), "the path of the script to run on jenkins, relative to repo root")
 	cmd.Flags().StringVar(&o.setupScript, "setup", os.Getenv(messages.RequestParameterSetupScript), "the path of the script to run on jenkins, before the run script, relative to repo root")
 	cmd.Flags().BoolVar(&o.multiNode, "multinode", false, "multinode is used to run tests on different nodes, see docs")
+	cmd.Flags().StringVar(&o.workdir, "workdir", "", "the work directory")
 	cmd.Flags().StringArrayVar(&o.envVarsArr, "env", []string{}, "additional env vars to expose to build and run scripts")
 	return cmd
 }
