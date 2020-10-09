c0476a1331e5effe92520d85b28f347c7427d4ca
Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>
2020-10-09 20:03:32 +0530

    Removing o.environ adding
    Signed-off-by: Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>

--- pkg/cli/worker/work.go
+++ pkg/cli/worker/work.go
@@ -48,14 +48,6 @@ func (wo *WorkOptions) envVarsArrToEnvVars() error {
 		}
 		wo.envVars[res[0]] = res[1]
 	}
-	osenv := os.Environ()
-	for _, item := range osenv {
-		res := strings.Split(item, "=")
-		if len(res) != 2 {
-			return fmt.Errorf("unable to split envvar, is it in the form FOO=BAR?")
-		}
-		wo.envVars[res[0]] = res[1]
-	}
 	return nil
 }
 
