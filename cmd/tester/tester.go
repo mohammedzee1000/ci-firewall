3c579879694eba66c82f336fd0d39ceb1c8d7982
Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>
2020-10-13 12:04:34 +0530

    Cleaning up tester cli
    Signed-off-by: Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>

--- cmd/tester/tester.go
+++ cmd/tester/tester.go
@@ -1,14 +1,5 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/mohammedzee1000/ci-firewall/pkg/node"
-)
-
 func main() {
-	nodes, _ := node.NodeListFromDir("test-nodes")
-	for _, n := range nodes {
-		fmt.Printf("%#v\n", n)
-	}
+	//for poc and testing purpose, do not commit
 }
