d7c89fd39b45b60504c0e8063e70ccfefac844c8
Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>
2020-10-21 09:55:19 +0530

    Some fixes
    Signed-off-by: Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>

--- scripts/prepare-release.sh
+++ scripts/prepare-release.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
 go build -mod=vendor -o dist/ci-firewall cmd/ci-firewall/ci-firewall.go
-tar -czf dist/ci-firewall.tar.gz dist/ci-firewall
+pushd dist
+tar -czf ci-firewall.tar.gz ci-firewall
+popd
