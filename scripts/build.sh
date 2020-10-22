fc9a2a8d0f604ad1fa47a46cd0aa279722a744af
Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>
2020-10-22 17:08:15 +0530

    Some more fixes
    Signed-off-by: Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-
 go build -mod=vendor -o dist/ci-firewall cmd/ci-firewall/ci-firewall.go
+go build -mod=vendor -o dist/ssh-run-cmd cmd/ssh-run-cmd/ssh-run-cmd.go
--- scripts/prepare-release.sh
+++ scripts/prepare-release.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 go build -mod=vendor -o dist/ci-firewall cmd/ci-firewall/ci-firewall.go
+go build -mod=vendor -o dist/ssh-run-cmd cmd/ssh-run-cmd/ssh-run-cmd.go
 pushd dist
 tar -czf ci-firewall.tar.gz ci-firewall
+tar -czf ssh-run-cmd.tar.gz ssh-run-cmd
 popd
