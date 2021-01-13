15862ecaf690bebbb6cdb06fd53565c1efc5f846
anandrkskd <anandrkskd@gmail.com>
2021-01-13 12:26:38 +0530

    adding rabbitmq setup

--- Dockerfiles/rabbitmq/rabbitmq-setup.sh
+++ Dockerfiles/rabbitmq/rabbitmq-setup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-(./usr/loacal/bin/docker-entrypoint.sh)&
+(/usr/local/bin/docker-entrypoint.sh rabbitmq-server)&
 sleep 5
 #login with guestuser
 rabbitmqctl add_user mqadmin mqadminpassword
@@ -8,13 +8,13 @@ rabbitmqctl set_permissions -p / mqadmin ".*" ".*" ".*"
 rabbitmq-plugins enable rabbitmq_jms_topic_exchange
 rabbitmqctl authenticate_user  mqadmin mqadminpassword
 
-sudo rabbitmq-plugins enable rabbitmq_management
+rabbitmq-plugins enable rabbitmq_management
 wget http://127.0.0.1:15672/cli/rabbitmqadmin
 chmod +x rabbitmqadmin
-mv rabbitmqadmin /etc/rabbitmq
-
+cp rabbitmqadmin /etc/rabbitmq
+cp rabbitmqadmin /usr/local/bin/
 
 #create a queue and exchange
 rabbitmqadmin declare exchange name=test-exchange type=fanout
 rabbitmqadmin declare queue name=test-queue
-rabbitmqadmin declare binding source=test-exchange destination=test-queue destination-type=queue routing_key=sendq
+rabbitmqadmin declare binding source=test-exchange destination=test-queue routing_key=sendq
