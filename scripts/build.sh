e98fc8d842ae6389f8fc6fcb5676900ffa43ecfc
Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>
2021-06-21 13:13:24 +0530

    Build statically linked binary
    Signed-off-by: Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,4 +4,4 @@ GITCOMMIT=${GITCOMMIT:-$(git rev-parse --short HEAD 2>/dev/null)}
 VERSION=${VERSION:="master"}
 rm -rf dist
 mkdir -p dist/build/linux/amd64
-go build -ldflags="-X ${PROJECT}/pkg/version.GITCOMMIT=${GITCOMMIT} -X ${PROJECT}/pkg/version.VERSION=${VERSION}" -mod=vendor -o dist/build/linux/amd64/ci-firewall cmd/ci-firewall/ci-firewall.go
+CGO_ENABLED=0 go build -ldflags="-extldflags=-static -X ${PROJECT}/pkg/version.GITCOMMIT=${GITCOMMIT} -X ${PROJECT}/pkg/version.VERSION=${VERSION}" -mod=vendor -o dist/build/linux/amd64/ci-firewall cmd/ci-firewall/ci-firewall.go
